69b00533c1e59109c4159c937526a9c6b383427b
nef734 <nefdevelop@gmail.com>
2025-09-24 15:27:09 +0200

    ✨ Cambiar el puerto de la aplicación a 8214 para evitar conflictos.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const session = require("express-session");
 const SQLiteStore = require("connect-sqlite3")(session);
 
 const app = express();
-const port = 3000;
+const port = 8214;
 
 const recipesPath = path.join(__dirname, "recetas");
 setupDatabase(db, recipesPath);
