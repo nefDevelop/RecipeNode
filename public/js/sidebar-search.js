3e3161da1930315d55593a1d2d8ce9332825eee0
nef734 <nefdevelop@gmail.com>
2026-02-06 08:14:03 +0100

    📝 Add recipe metadata display: meal type, rating, views, description, and equipment with icons and tooltips

--- public/js/sidebar-search.js
+++ public/js/sidebar-search.js
@@ -58,12 +58,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (displaySettings.name) {
       cardContentHtml += `
-          <h3
-            class="font-semibold text-lg text-gray-800 dark:text-gray-200 group-hover:text-green-600 truncate"
-            title="${recipe.name}"
-          >
-            ${recipe.name}
-          </h3>
+          <div class="flex items-center gap-2 mb-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 dark:text-gray-400 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
+            </svg>
+            <h3
+              class="font-semibold text-lg text-gray-800 dark:text-gray-200 group-hover:text-green-600 truncate min-w-0"
+              title="${recipe.name}"
+            >
+              ${recipe.name}
+            </h3>
+          </div>
         `;
     }
 
@@ -106,6 +111,40 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
     }
 
+    if (displaySettings.mealType && recipe.meal_type) {
+      cardContentHtml += `
+          <div class="meta-item" title="Tipo de comida">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
+            </svg>
+            <span>${recipe.meal_type}</span>
+          </div>
+        `;
+    }
+
+    if (displaySettings.rating && recipe.rating) {
+      cardContentHtml += `
+          <div class="meta-item" title="Puntuación">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+            </svg>
+            <span>${recipe.rating}</span>
+          </div>
+        `;
+    }
+
+    if (displaySettings.views && recipe.views !== undefined) {
+      cardContentHtml += `
+          <div class="meta-item" title="Vistas">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+            </svg>
+            <span>${recipe.views}</span>
+          </div>
+        `;
+    }
+
     if (displaySettings.tags && recipe.tags && recipe.tags.length > 0) {
       cardContentHtml += `
           <div class="meta-item" title="Etiquetas">
@@ -125,6 +164,17 @@ document.addEventListener("DOMContentLoaded", () => {
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
             </svg>
             <span class="truncate max-w-[150px]">${ingredients}</span>
+          </div>
+        `;
+    }
+
+    if (displaySettings.description && recipe.description) {
+      cardContentHtml += `
+          <div class="meta-item col-span-full" title="Descripción">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+            </svg>
+            <span class="truncate">${recipe.description}</span>
           </div>
         `;
     }
