d4dbcfb7922a1ce1bbe1b02ea3525f54f958ff37
nef734 <nefdevelop@gmail.com>
2026-02-06 08:00:34 +0100

    ✅ Update cuisine type icon and SVG attributes for better accessibility and compatibility

--- public/js/sidebar-search.js
+++ public/js/sidebar-search.js
@@ -95,8 +95,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (displaySettings.cuisineType && recipe.cuisine_type) {
       cardContentHtml += `
           <div class="meta-item" title="Tipo de cocina">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M21.54 15H17a2 2 0 0 0-2 2v4.54"/>
+              <path d="M7 3.34V5a3 3 0 0 0 3 3a2 2 0 0 1 2 2c0 1.1.9 2 2 2a2 2 0 0 0 2-2c0-1.1.9-2 2-2h3.17"/>
+              <path d="M11 21.95V18a2 2 0 0 0-2-2a2 2 0 0 1-2-2v-1a2 2 0 0 0-2-2H2.05"/>
+              <circle cx="12" cy="12" r="10"/>
             </svg>
             <span>${recipe.cuisine_type}</span>
           </div>
